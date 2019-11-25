98ff7b7fbe5c11e3150d3b6d56d1836b19ce5405
unknown <matthewaustinbell@gmail.com>
2019-11-25 17:14:43 -0600

    wip

--- StudentLoans/DataAccess/UserRepository.cs
+++ StudentLoans/DataAccess/UserRepository.cs
@@ -1,8 +1,33 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentLoans.Models;
+using System.Data.SqlClient;
+using Dapper;
 
-public class Class1
+public class UserRepository
 {
 	public Class1()
 	{
 	}
+}
+
+
+namespace StudentLoans.DataAccess
+{
+    public class UserAuthentication
+    {
+        string _connectionString = "Server=localhost;Database=StudentLoans;Trusted_Connection=True;";
+
+        public IEnumerable<User> GetAllUserss()
+        {
+            //using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = "Select count(*) from Users where Id = @firebaseId;";
+                var users  = db.Query<Users>(sql);
+                return users;
+            }
+        }
+
+    }
 }
