b0d3bc9ce9443f121cea62891ea88442f7a1b197
Heath Moore <heathjhmoore@gmail.com>
2019-12-01 19:09:29 -0600

    created function to gather all rental history for product owner

---
+++ StudentLoans/DataAccess/RentalRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentLoans.Models;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace StudentLoans.DataAccess
+{
+    public class RentalRepository
+    {
+        string _connectionString = "Server=localhost;Database=StudentLoans;Trusted_Connection=True;";
+
+        public IEnumerable<Rental> GetRentalsByProductOwner(string ownerFirebaseId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"
+                        SELECT 
+                            r.Id,
+						    r.ProductId,
+						    r.RenterId,
+						    r.TotalCost,
+						    r.PaymentTypeId,
+						    r.StartDate,
+						    r.ReturnedDate,
+						    p.[Name] as ProductName,
+						    p.PricePerDay,
+						    CONCAT(u.FirstName, ' ', u.LastName) as OwnerFullName
+                        FROM Rentals r
+                        LEFT JOIN Products p on p.Id = r.ProductId
+                        LEFT JOIN Users u on u.Id = p.OwnerId
+                        WHERE u.FireBaseId = @FirebaseId";
+                var param = new { FirebaseId = ownerFirebaseId };
+                var OwnerRentals = db.Query<Rental>(sql, param);
+                return OwnerRentals;
+            }
+        }
+    }
+}
--- StudentLoans/Models/Rental.cs
+++ StudentLoans/Models/Rental.cs
@@ -7,6 +7,15 @@ namespace StudentLoans.Models
 {
     public class Rental
     {
-
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int RenterId { get; set; }
+        public decimal TotalCost {get; set;}
+        public int PaymentTypeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime ReturnedDate { get; set; }
+        public string ProductName { get; set; }
+        public decimal PricePerDay { get; set; }
+        public string OwnerFullName { get; set; }
     }
 }
