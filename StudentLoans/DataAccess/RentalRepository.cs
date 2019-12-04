829dfd8b9d9b6b859e2b206d12d48c435323ad71
Heath Moore <heathjhmoore@gmail.com>
2019-12-03 18:37:04 -0600

    created function to allow user to see their rental history

--- StudentLoans/DataAccess/RentalRepository.cs
+++ StudentLoans/DataAccess/RentalRepository.cs
@@ -22,15 +22,17 @@ namespace StudentLoans.DataAccess
 						    r.ProductId,
 						    r.RenterId,
 						    r.TotalCost,
-						    r.PaymentTypeId,
+                            c.Type as PaymentType,
 						    r.StartDate,
 						    r.ReturnedDate,
 						    p.[Name] as ProductName,
+                            p.ImageUrl,
 						    p.PricePerDay,
 						    CONCAT(u.FirstName, ' ', u.LastName) as OwnerFullName
                         FROM Rentals r
                         LEFT JOIN Products p on p.Id = r.ProductId
                         LEFT JOIN Users u on u.Id = p.OwnerId
+                        LEFT JOIN CustomerPaymentTypes c on c.Id = r.PaymentTypeId
                         WHERE u.FireBaseId = @FirebaseId";
                 var param = new { FirebaseId = ownerFirebaseId };
                 var OwnerRentals = db.Query<Rental>(sql, param);
--- StudentLoans/Models/Rental.cs
+++ StudentLoans/Models/Rental.cs
@@ -11,10 +11,11 @@ namespace StudentLoans.Models
         public int ProductId { get; set; }
         public int RenterId { get; set; }
         public decimal TotalCost {get; set;}
-        public int PaymentTypeId { get; set; }
+        public string PaymentType { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime ReturnedDate { get; set; }
         public string ProductName { get; set; }
+        public string ImageUrl { get; set; }
         public decimal PricePerDay { get; set; }
         public string OwnerFullName { get; set; }
     }
